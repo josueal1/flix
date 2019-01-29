8b1feb67f34ba1ccfa1e675ae7a2cad5d1c9bd3a
Josue Lopez <josueal1@uci.edu>
2019-01-28 21:09:58 -0800

    Rearranged Certain Assets Files Centered the Launch Screen Icon Added a Grid View Layout

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Work on 1/27/19.
+//  Copyright © 2019 galvez2j. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie  = [String:Any]!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+ //
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Work on 1/28/19.
+//  Copyright © 2019 galvez2j. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
